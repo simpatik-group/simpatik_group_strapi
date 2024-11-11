ac6dfc487f17a1abfb15ce4b059d2bdf86f8c385
Maksym Didyk <didyk.maksym@gmail.com>
2024-11-11 22:18:42 +0200

    Updated to v5.3.0

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,45 +1,55 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface TeamDepartament extends Struct.ComponentSchema {
-  collectionName: 'components_team_departaments';
+export interface CareerVacancies extends Struct.ComponentSchema {
+  collectionName: 'components_career_vacancies';
   info: {
-    displayName: 'departament';
     description: '';
+    displayName: 'vacancies';
   };
   attributes: {
-    image: Schema.Attribute.Media<'images', true>;
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface HeaderHeaderMenu extends Struct.ComponentSchema {
+  collectionName: 'components_header_header_menus';
+  info: {
+    description: '';
+    displayName: 'menu';
+    icon: 'layer';
+  };
+  attributes: {
     title: Schema.Attribute.String & Schema.Attribute.Required;
+    url: Schema.Attribute.String;
   };
 }
 
-export interface LocationsLocations extends Struct.ComponentSchema {
-  collectionName: 'components_locations_locations';
+export interface HomepageFeedbacks extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_feedbacks';
   info: {
-    displayName: 'locations';
     description: '';
+    displayName: 'feedbacks';
+    icon: 'write';
   };
   attributes: {
-    title: Schema.Attribute.String;
-    text: Schema.Attribute.String;
-    position_homepage: Schema.Attribute.Integer &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 4;
-        },
-        number
-      >;
+    feedback_company: Schema.Attribute.String;
+    feedback_content: Schema.Attribute.Text;
+    feedback_person: Schema.Attribute.String;
+    feedback_photo: Schema.Attribute.Media<'images', true>;
   };
 }
 
-export interface LifeAdvantages extends Struct.ComponentSchema {
-  collectionName: 'components_life_advantages';
+export interface HomepageHomepageNumbers extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_homepage_numbers';
   info: {
-    displayName: 'advantages';
+    description: '';
+    displayName: 'numbers';
   };
   attributes: {
-    title: Schema.Attribute.String;
-    text: Schema.Attribute.Text;
+    number: Schema.Attribute.String;
+    text: Schema.Attribute.String;
   };
 }
 
@@ -51,70 +61,60 @@ export interface HomepagePartnerLogos extends Struct.ComponentSchema {
   attributes: {};
 }
 
-export interface HomepageHomepageNumbers extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_homepage_numbers';
+export interface LifeAdvantages extends Struct.ComponentSchema {
+  collectionName: 'components_life_advantages';
   info: {
-    displayName: 'numbers';
-    description: '';
+    displayName: 'advantages';
   };
   attributes: {
-    number: Schema.Attribute.String;
-    text: Schema.Attribute.String;
-  };
-}
-
-export interface HomepageFeedbacks extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_feedbacks';
-  info: {
-    displayName: 'feedbacks';
-    icon: 'write';
-    description: '';
-  };
-  attributes: {
-    feedback_content: Schema.Attribute.Text;
-    feedback_person: Schema.Attribute.String;
-    feedback_company: Schema.Attribute.String;
-    feedback_photo: Schema.Attribute.Media<'images', true>;
-  };
-}
-
-export interface HeaderHeaderMenu extends Struct.ComponentSchema {
-  collectionName: 'components_header_header_menus';
-  info: {
-    displayName: 'menu';
-    icon: 'layer';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String & Schema.Attribute.Required;
-    url: Schema.Attribute.String;
-  };
-}
-
-export interface CareerVacancies extends Struct.ComponentSchema {
-  collectionName: 'components_career_vacancies';
-  info: {
-    displayName: 'vacancies';
-    description: '';
-  };
-  attributes: {
+    text: Schema.Attribute.Text;
     title: Schema.Attribute.String;
-    description: Schema.Attribute.Text;
-    url: Schema.Attribute.String;
+  };
+}
+
+export interface LocationsLocations extends Struct.ComponentSchema {
+  collectionName: 'components_locations_locations';
+  info: {
+    description: '';
+    displayName: 'locations';
+  };
+  attributes: {
+    position_homepage: Schema.Attribute.Integer &
+      Schema.Attribute.SetMinMax<
+        {
+          max: 4;
+          min: 1;
+        },
+        number
+      >;
+    text: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface TeamDepartament extends Struct.ComponentSchema {
+  collectionName: 'components_team_departaments';
+  info: {
+    description: '';
+    displayName: 'departament';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images', true>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'team.departament': TeamDepartament;
-      'locations.locations': LocationsLocations;
-      'life.advantages': LifeAdvantages;
-      'homepage.partner-logos': HomepagePartnerLogos;
-      'homepage.homepage-numbers': HomepageHomepageNumbers;
-      'homepage.feedbacks': HomepageFeedbacks;
-      'header.header-menu': HeaderHeaderMenu;
       'career.vacancies': CareerVacancies;
+      'header.header-menu': HeaderHeaderMenu;
+      'homepage.feedbacks': HomepageFeedbacks;
+      'homepage.homepage-numbers': HomepageHomepageNumbers;
+      'homepage.partner-logos': HomepagePartnerLogos;
+      'life.advantages': LifeAdvantages;
+      'locations.locations': LocationsLocations;
+      'team.departament': TeamDepartament;
     }
   }
 }

53015245d9cb793fde8df0c9e8b69df3dca8b50e
Maksym Didyk <didyk.maksym@gmail.com>
2025-01-21 18:08:06 +0200

    #2873 and #2874 Add locale in Team middleware; Restrict charity and charity-page routes to specific actions

--- src/api/charity-page/routes/charity-page.ts
+++ src/api/charity-page/routes/charity-page.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::charity-page.charity-page');
+export default factories.createCoreRouter('api::charity-page.charity-page', {
+  only: ['find'],
+});
--- src/api/charity/routes/charity.ts
+++ src/api/charity/routes/charity.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::charity.charity');
+export default factories.createCoreRouter('api::charity.charity', {
+  only: ['find', 'findOne'],
+});

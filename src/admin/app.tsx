f744253cd6b5076b5275b91e61dc4d78f8688f3b
Maksym Didyk <didyk.maksym@gmail.com>
2025-02-28 19:37:29 +0200

    Update page title for admin panel

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -36,10 +36,32 @@ export default {
       en: {
         "Auth.form.welcome.title": "Simpatik Group",
         "Auth.form.welcome.subtitle": "Log in",
+        "Settings.profile.form.section.experience.interfaceLanguageHelp": "Preference changes will apply only to you",
+      },
+      uk: {
+        "Auth.form.welcome.subtitle": "Вхід",
+        "Auth.form.rememberMe.label": "Запамятати мене",
+        "HomePage.header.title": "Вітаємо {name}",
+        "HomePage.header.subtitle": "Ласкаво просимо до панелі адміністрування",
+        "global.save": "Зберегти",
+        "global.select": "Оберіть",
+        "global.password": "Пароль",
+        "global.finish": "Завершити",
       },
     },
   },
   bootstrap(app: StrapiApp) {
+    document.title = "Simpatik Group Admin";
     // console.log(app);
+    const observer = new MutationObserver(() => {
+      if (document.title !== "Simpatik Group Admin") {
+        document.title = "Simpatik Group Admin";
+      }
+    });
+
+    observer.observe(document.querySelector("title"), {
+      childList: true,
+      subtree: true,
+    });
   },
 };
